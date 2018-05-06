ed2ef3717ae9d76be9b71e49eb89be093cfa2bae
Abhishek <aeiouisvowel@gmail.com>
2018-05-07 00:18:25 +0530

    added ajax on contact me

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 $(document).ready(() => {
 
-    $('.js--about').waypoint((direction)=>{
+    $('.js--about').waypoint((direction) => {
         if (direction == "down") {
             $('nav').addClass('sticky');
         }
         else {
             $('nav').removeClass('sticky');
         }
-    },{
-        offset: '80px'
-    });
+    }, {
+            offset: '80px'
+        });
 
-    $('nav').waypoint((direction)=>{
+    $('nav').waypoint((direction) => {
         if (direction == "down") {
             $('.resume-contents').css({
                 'border': 'none',
-                'color' : '#950740',
-                'background-color':'rgba(255,255,255,0.7)',
+                'color': '#950740',
+                'background-color': 'rgba(255,255,255,0.7)',
                 'transition': 'border 0.2s, color 0.2s, background-color 0.2s'
             });
             $('.resume-download').css({
@@ -29,9 +29,9 @@ $(document).ready(() => {
         else {
             $('.resume-contents').css({
                 'border': '4px solid #fff',
-                'color' : '#eee',
-                'background-color':'transparent',
-                'transition': 'border 0.2s, color 0.2s, background-color 0.2s'                
+                'color': '#eee',
+                'background-color': 'transparent',
+                'transition': 'border 0.2s, color 0.2s, background-color 0.2s'
             });
             $('.resume-download').css({
                 'border': '2px solid whitesmoke',
@@ -40,10 +40,33 @@ $(document).ready(() => {
                 'transition': 'border 0.2s, color 0.2s, background-color 0.2s, padding 0.2s'
             });
         }
-    },{
-        offset: '450px'
-    });
+    }, {
+            offset: '450px'
+        });
 
+    var noOfmails = 0;
+    $('.contact-button').click(() => {
+        var contactName = document.getElementById('contact-name').value;
+        var email = document.getElementById('email').value;
+        var message = document.getElementById('message').value;
+        console.log(contactName);
+        console.log(email);
+        console.log(message);
+
+        var xhttp = new XMLHttpRequest();
+
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                // document.getElementById("demo").innerHTML = this.responseText;
+                console.log(this.responseText);
+            }
+        };
+
+        xhttp.open("POST", "https://afternoon-stream-61459.herokuapp.com/sendMail", true);
+        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhttp.send("contactName="+contactName+"&email="+email+"&message="+message);
+
+    })
 
     // $('.js-clickAbout').click(function() {
     //     $('html,body').animate({scrollTop: $('.js--about').offset.top}, 1000)
